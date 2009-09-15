aad4df6c5b6e674060b3f864a4d9307afb910315
Reza Jelveh <toyb0mb@gmx.net>
2009-09-15 11:57:04 +0200

    authhelper changes

---
+++ MoreAuthHelper/AuthHelper.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MoreAuthenticationTest
+//
+//  Created by Reza Jelveh on 9/14/09.
+//  Copyright 2009 Flying Seagull. All rights reserved.
+//
+
+
+int main(int argc, char **argv){
+    
+    NSString *stringTest = [[NSString alloc] initWithString:@"test"];
+    return 0;
+}
