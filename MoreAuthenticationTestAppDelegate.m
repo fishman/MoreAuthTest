ce3190885d00f9e07a276f49df68b45c3973d0f8
Reza Jelveh <toyb0mb@gmx.net>
2009-09-16 00:27:42 +0200

    run moreauthhelper from main app

--- MoreAuthenticationTestAppDelegate.m
+++ MoreAuthenticationTestAppDelegate.m
@@ -17,6 +17,16 @@
 }
 
 - (void)testOpen:(id)sender {
+    NSString *rootPath = [[NSBundle mainBundle] resourcePath];
+    NSString *filePath = [rootPath stringByAppendingPathComponent:@"MoreAuthHelper"];
+    NSTask *helper = [[NSTask alloc] init];
+
+    [helper setLaunchPath:filePath];
+    [helper setArguments:[NSArray arrayWithObjects:@"world", @"test", nil]];
+    [helper launch];
+    [helper waitUntilExit];
+    [helper release];
+
     NSLog(@"hello world");
 }
 
